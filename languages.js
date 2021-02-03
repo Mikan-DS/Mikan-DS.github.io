deabbad30bb636a802e9692e5f044cafa4b2f1f7
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2021-02-04 00:01:30 +0300

    English added

--- languages.js
+++ languages.js
@@ -1,5 +1,11 @@
 function RussianVersion() {
-  document.getElementById("russian_lang_text").display = None;
+  document.getElementById("russian_lang_text").style.display = "block";
+  document.getElementById("english_lang_text").style.display = "none";
   
+ }
+ 
+ function EnglishVersion() {
+  document.getElementById("russian_lang_text").style.display = "none";
+  document.getElementById("english_lang_text").style.display = "block";
   
  }
